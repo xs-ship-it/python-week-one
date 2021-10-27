c57ab763a2ca9a3e12e43b2ac589549dd59d10c2
Felix Nderitu <newtnfelix@gmail.com>
2021-10-27 08:17:05 +0300

    create a unittestfile for the user

---
+++ user_test.py
@@ -0,0 +1,7 @@
+import unittest # Importing the unittest module
+from user import User
+
+class TestUser(unittest.TestCase):
+    """
+    Test class that defines test cases for the user class behaviours.
+    """
