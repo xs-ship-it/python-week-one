27135f4a694b9e51479f7dc40597af97d624601c
Felix Nderitu <newtnfelix@gmail.com>
2021-10-27 08:19:33 +0300

    make a unittest class

--- user_test.py
+++ user_test.py
@@ -4,4 +4,6 @@ from user import User
 class TestUser(unittest.TestCase):
     """
     Test class that defines test cases for the user class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
     """
