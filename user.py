469e88eb71c36d2f44c8fe432b26de3d56a2328a
Felix Nderitu <newtnfelix@gmail.com>
2021-10-27 07:59:39 +0300

    add user class

---
+++ user.py
@@ -0,0 +1,14 @@
+class User:
+    """
+    Class that generates new instances of contacts.
+    """
+
+    def __init__(self,account_name,user_name,password):
+
+      # docstring removed for simplicity
+
+        self.account_name = account_name
+        self.user_name = user_name
+        self.password = password
+    
+        
