38b0212ebf2a44573450af0740efaf17dc30432d
Felix Nderitu <newtnfelix@gmail.com>
2021-10-28 14:05:41 +0300

    create a delete user function

--- user.py
+++ user.py
@@ -11,4 +11,20 @@ class User:
         self.user_name = user_name
         self.password = password
     
-        
+    contact_list = [] # Empty contact list
+ 
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self) 
+
+    def delete_user(self):
+
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+
+        User.user_list.remove(self)       
--- user_test.py
+++ user_test.py
@@ -7,3 +7,53 @@ class TestUser(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     """
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("google","kim","5927") 
+
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.account_name,"google")
+        self.assertEqual(self.new_user.user_name,"kim")
+        self.assertEqual(self.new_user.password,"5927")
+
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_list),1)    
+
+    def test_save_multiple_user(self):
+            '''
+            test_save_multiple_users to check if we can save multiple user
+            objects to our user_list
+            '''
+            self.new_user.save_user()
+            test_user = User("Test","kim","5927") # new user
+            test_user.save_user()
+            self.assertEqual(len(User.user_list),2)
+    def test_delete_contact(self):
+            '''
+            test_delete_user to test if we can remove a user from our user list
+            '''
+            self.new_user.save_user()
+            test_user = User("Test""user","kim","5927") # new user
+            test_user.save_user()
+
+            self.new_user.delete_user()# Deleting a user object
+            self.assertEqual(len(User.user_list),1)        
+if __name__ == '__main__':
+    unittest.main()
