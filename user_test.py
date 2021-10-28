b500bc6cf3caeab00a993bb4e2399adadd62cb4b
Felix Nderitu <newtnfelix@gmail.com>
2021-10-28 20:44:51 +0300

    add a display users test

--- user_test.py
+++ user_test.py
@@ -80,6 +80,13 @@ class TestUser(unittest.TestCase):
 
         user_exists =User.user_exist("google")
 
-        self.assertTrue(user_exists)             
+        self.assertTrue(user_exists) 
+
+    def test_display_all_users(self):
+        '''
+        method that returns a list of all users saved
+        '''
+
+        self.assertEqual(User.display_users(),User.user_list)           
 if __name__ == '__main__':
     unittest.main()
