04f912537232797536c74dce78c47b95d13a5424
Felix Nderitu <newtnfelix@gmail.com>
2021-10-29 04:45:18 +0300

    create a create user function in the run.py file

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3.8
 from user import User
+
+def create_user(account_name,user_name,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(account_name,user_name,password)
+    return new_user
